50bfe1b46de07b3d4a168be4abf705c64857739d
Mia Reimer <mia.reimer@googlemail.com>
2023-09-15 16:09:46 +0200

    Update api with update and delete options

--- API/main.py
+++ API/main.py
@@ -21,7 +21,7 @@ def get_db():
 def index():
     return {"data": "house list"}
 
-# get all user
+# get all houses
 @app.get("/houses", response_model=list[schemas.HouseModel])
 def get_all_houses(db: Session = Depends(get_db)):
     house = db.query(models.House).all()
@@ -30,7 +30,7 @@ def get_all_houses(db: Session = Depends(get_db)):
     return house
 
 
-# add user
+# add house
 @app.post("/houses", response_model=schemas.HouseModel)
 def create_house(request: schemas.HouseModel, db: Session = Depends(get_db)):
     new_house = models.House(id=request.id, price=request.price, bedrooms=request.bedrooms, 
@@ -39,3 +39,25 @@ def create_house(request: schemas.HouseModel, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_house)
     return new_house
+
+
+# update house
+@app.put("/houses/{id}")
+def update_houses(id: int, request: schemas.HouseModel, db: Session = Depends(get_db)):
+    house = db.query(models.House).filter(models.House.id == id)
+    if not house.first():
+        raise HTTPException(status_code=404, detail="House not found")
+    house.update(request.dict())
+    db.commit()
+    return "Updated successfully"
+
+
+# delete house
+@app.delete("/houses/{id}")
+def delete(id: int, db: Session = Depends(get_db)):
+    house = db.query(models.House).filter(models.House.id == id)
+    if not house.first():
+        raise HTTPException(status_code=404, detail="House not found")
+    house.delete(synchronize_session=False)
+    db.commit()
+    return "Deleted house successfully"
